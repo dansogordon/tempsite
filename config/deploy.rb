8786ccc27d993ff43ba53d2a060574531a20d631
NEHA SUWAl <neha@jyaasa.com>
2015-06-12 10:27:18 +0545

    Modified the repo_url for deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'NodeBomb'
-set :repo_url, 'github.com/dansogordon/tempsite.git'
+set :repo_url, 'git@github.com:dansogordon/tempsite.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
