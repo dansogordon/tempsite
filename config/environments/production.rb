855e24e90a933afa649e5a4d8bfad561c4c6ed38
gkunwar <gkunwar09@gmail.com>
2015-08-21 14:50:43 +0545

    Added host name to production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,7 @@ Rails.application.configure do
   # Mailer setting
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'http://staging.kapilrajnakhwa.com' }
   #Options for smtp delivery
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
