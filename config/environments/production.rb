a145960b1b2678f61a5a5beea096c0b49d95fc8a
gkunwar <gkunwar09@gmail.com>
2015-08-21 15:03:58 +0545

    Fixed problem related to email security in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: "587",
-    enable_starttls_auto:  false,
     authentication: :plain,
     user_name: ENV["email"],
     password: ENV["password"]
